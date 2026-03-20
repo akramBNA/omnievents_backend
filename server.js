37168774f59e64af639407ecd66c402a64da779f
Akram <akrambenaoun1993@gmail.com>
2026-03-20 14:00:26 +0100

    UPDATE server.js file: add a health check route

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// const { sequelize } = require('./database/neon_db');
-const db = process.env.DB_PROVIDER === "local" ? require("./database/local_db") : require("./database/neon_db");
+const db =
+  process.env.DB_PROVIDER === "local"
+    ? require("./database/local_db")
+    : require("./database/neon_db");
 
 const { sequelize } = db;
 
@@ -14,22 +16,16 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/health", async (req, res) => {
-  try {
-    await sequelize.authenticate();
-    res.json({ status: "OK", db: "connected" });
-  } catch {
-    res.status(500).json({ status: "ERROR", db: "disconnected" });
-  }
-});
-
 app.get("/", (req, res) => {
   res.send("Hello from your omnievents Node.js backend!");
 });
 
+let isDbConnected = false;
+
 const startServer = async () => {
   try {
     await sequelize.authenticate();
+    isDbConnected = true;
     console.log("Database connected successfully!");
 
     app.listen(port, () => {
@@ -40,5 +36,12 @@ const startServer = async () => {
     process.exit(1);
   }
 };
+
+app.get("/health", (req, res) => {
+  if (isDbConnected) {
+    return res.json({ status: "OK", db: "connected" });
+  }
+  return res.status(500).json({ status: "ERROR", db: "disconnected" });
+});
 
 startServer();

01f233a7267356da573d1748253e92233862e99b
Akram <akrambenaoun1993@gmail.com>
2026-03-20 19:54:13 +0100

    CREATE events model

--- models/events.models.js
+++ models/events.models.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const useNeon = process.env.DB_PROVIDER === 'neon';
-const { sequelize } = useNeon ? require('../database/neon_db') : require('../database/local_db');
+const useNeon = process.env.DB_PROVIDER === "neon";
+const { sequelize } = useNeon ? require("../database/neon_db") : require("../database/local_db");
 
 const events = sequelize.define(
   "events",
@@ -14,18 +14,31 @@ const events = sequelize.define(
     event_name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     event_details: {
       type: DataTypes.TEXT,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     event_start_date: {
-      type: DataTypes.DATE,
+      type: DataTypes.DATEONLY,
       allowNull: false,
     },
     event_end_date: {
-      type: DataTypes.DATE,
+      type: DataTypes.DATEONLY,
       allowNull: false,
+      validate: {
+        isValidDateRange() {
+          if (this.event_end_date < this.event_start_date) {
+            throw new Error("End date cannot be before start date");
+          }
+        },
+      },
     },
     active: {
       type: DataTypes.BOOLEAN,
@@ -35,7 +48,7 @@ const events = sequelize.define(
   },
   {
     timestamps: false,
-  }
+  },
 );
 
 module.exports = { events, sequelize };

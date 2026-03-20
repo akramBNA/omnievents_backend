1ddbadb7eca0d21bf7aa7365638adff9666c4e8e
Akram <akrambenaoun1993@gmail.com>
2026-03-20 14:03:36 +0100

    cleanUp local_db file

--- database/local_db.js
+++ database/local_db.js
@@ -1,30 +1,15 @@
-const { Pool } = require('pg');
+const { Sequelize } = require("sequelize");
 
-const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
+  },
+);
 
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost', 
-  dialect: 'postgres',
-});
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
-pool.connect((err) => {
-  if (err) {
-    console.error('Connection error', err.stack);
-  } else {
-    console.log('Connected to the local database');
-  }
-});
-
-module.exports = {
-  pool,
-  sequelize
-};
+module.exports = { sequelize };

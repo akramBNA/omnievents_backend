b75a5efbee343836fc5090483f5f83a9ffe44682
Akram <akrambenaoun1993@gmail.com>
2026-03-20 15:12:24 +0100

    ADD get,signup, signin routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-// const users_controller = require("../controllers/users.controllers");
+const users_controller = require("../controllers/users.controllers");
 const roles_controller = require("../controllers/roles.controllers");
 
 // ROLES ROUTES:
 router.get("/roles/getAllRoles/", roles_controller.getAllRoles);
 router.post("/roles/CreateRole/", roles_controller.createRole);
+
+// USERS ROUTES:
+router.get("/users/getAllUsers/", users_controller.getAllUsers);
+router.post("/users/signUp/", users_controller.signUp);
+router.post("/users/signIn/", users_controller.signIn);
 
 module.exports = router;

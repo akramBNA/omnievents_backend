c299fd535079f70a3794bc59aaa7b71f2117d900
Akram <akrambenaoun1993@gmail.com>
2026-03-20 19:41:29 +0100

    ADD authentication token to protect the routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const authenticateToken = require('../middleware/authentication.middleware');
+
+
 const users_controller = require("../controllers/users.controllers");
 const roles_controller = require("../controllers/roles.controllers");
 
 // ROLES ROUTES:
-router.get("/roles/getAllRoles/", roles_controller.getAllRoles);
-router.post("/roles/CreateRole/", roles_controller.createRole);
+router.get("/roles/getAllRoles/",authenticateToken, roles_controller.getAllRoles);
+router.post("/roles/CreateRole/", authenticateToken, roles_controller.createRole);
 
 // USERS ROUTES:
-router.get("/users/getAllUsers/", users_controller.getAllUsers);
-router.post("/users/signUp/", users_controller.signUp);
-router.post("/users/signIn/", users_controller.signIn);
+router.get("/users/getAllUsers/", authenticateToken, users_controller.getAllUsers);
+router.post("/users/signUp/", authenticateToken, users_controller.signUp);
+router.post("/users/signIn/", authenticateToken, users_controller.signIn);
 
 module.exports = router;

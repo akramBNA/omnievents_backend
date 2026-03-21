ed44b573c745dcb00acc4d9a6cd8def9ec73a5f5
Akram <akrambenaoun1993@gmail.com>
2026-03-21 09:06:00 +0100

    CREATE users_events DB model

---
+++ models/users_events.models.js
@@ -0,0 +1,50 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const { roles } = require("./roles.models");
+const { users } = require("./users.models");
+const { events } = require("./events.models");
+const useNeon = process.env.DB_PROVIDER === "neon";
+const { sequelize } = useNeon
+  ? require("../database/neon_db")
+  : require("../database/local_db");
+
+const users_events = sequelize.define(
+  "users_events",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: users,
+        key: "user_id",
+      },
+    },
+    event_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: events,
+        key: "event_id",
+      },
+    },
+    subscribed_at: {
+      type: DataTypes.timestamps,
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    timestamps: false,
+  },
+);
+
+module.exports = { users_events, sequelize };
